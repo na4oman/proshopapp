ebe4a5d36bdeebaf67b7283f9462e4d0ce0baeba
na4oman <atanas.irikev@gmail.com>
2020-11-23 18:08:06 +0200

    add paypal payments script and button

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -9,7 +9,7 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
-  ORDER_PAY_RESET,
+  // ORDER_PAY_RESET,
 } from '../constants/orderConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
