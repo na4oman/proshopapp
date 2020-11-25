6c89b2cfe1a39a77cc1df8c9d4086d0ee8c2ffdb
na4oman <atanas.irikev@gmail.com>
2020-11-25 14:12:20 +0200

    Add admin delete backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
